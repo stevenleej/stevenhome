8e3f5c52681a6fea51ed1b506c17a184e776b129
Steven Lee <koreancoffe@gmail.com>
2021-01-30 06:25:10 -0500

    react

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './Navbar/nav';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/Navbar/nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <Nav>
+                <NavbarContainer>
+                    
+                </NavbarContainer>
+            </Nav>
+        </div>
+    );
+};
+
+export default Navbar;
